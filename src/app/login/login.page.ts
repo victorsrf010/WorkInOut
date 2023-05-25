8820d4f57a48df065686cdb52b6c6509c03ee4de
Vitor Ferreira <victorsrf010@gmail.com>
2023-05-25 11:48:30 +0100

    Login update

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
+
+    toHome() {
+      this.route.navigate(['/home']);
+    }
+
+    toRegister() {
+      this.route.navigate(['/registo'])
+    }
+
 
   ngOnInit() {
   }
